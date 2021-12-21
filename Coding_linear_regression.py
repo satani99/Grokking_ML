9729b8a5ea4818193e1ca05f1720691277664bad
satani99 <42287151+satani99@users.noreply.github.com>
2021-12-21 09:39:44 +0530

    Add files via upload

--- Coding_linear_regression.py
+++ Coding_linear_regression.py
@@ -14,4 +14,18 @@ def absolute_trick(base_price, price_per_room, num_rooms, price, learning_rate):
         base_price -= learning_rate
     return base_price, price_per_room
 
+
+import random
+
+def linear_regression(features, labels, learning_rate=0.01, epochs=1000):
+    price_per_room = random.random()
+    base_price = random.random()
+    for epoch in range(epochs):
+        i = random.randint(0, len(features)-1)
+        num_rooms = features[i]
+        price = labels[i]
+        price_per_room, base_price = square_trick(base_price, price_per_room, num_rooms, price, learning_rate=learning_rate)
+
+    return price_per_room, base_price
+
     
